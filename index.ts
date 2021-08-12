3ba29f4df0690c2097fd9d602319f0086296ae68
isabeela <isabeelabg@gmail.com>
2021-08-12 17:16:11 -0300

    2 files modified

--- index.ts
+++ index.ts
@@ -1,7 +1,18 @@
-let title: string = "MyApp"
+let title: string = 'MyApp';
 
-let paragraph: string = "Blablablabla...."
+let paragraph: string = 'Blablablabla....';
+
+let counter = 0;
+
+let intervalId = setInterval(() => {
+  counter = counter + 1;
+  const appCounter: HTMLElement = document.getElementById('app.counter');
+  appCounter.innerHTML = counter + '';
+}, 1000);
 
 // Write TypeScript code!
-const appDiv: HTMLElement = document.getElementById('app.title');
-appDiv.innerHTML = title;
+const appTitle: HTMLElement = document.getElementById('app.title');
+appTitle.innerHTML = title;
+
+const appParagraph: HTMLElement = document.getElementById('app.paragraph');
+appParagraph.innerHTML = paragraph;
