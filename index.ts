336396b2acfafbcd3212ac0c3bb34fffa6c1293e
isabeela <isabeelabg@gmail.com>
2021-08-12 18:22:36 -0300

    1 file modified, 1 added

--- index.ts
+++ index.ts
@@ -1,3 +1,5 @@
+import { Greeter } from './greeter';
+
 let title: string = 'MyApp';
 
 let paragraph: string = 'Blablablabla....';
@@ -10,9 +12,17 @@ let intervalId = setInterval(() => {
   appCounter.innerHTML = counter + '';
 }, 1000);
 
+const btn = document.getElementById('app.btn');
+btn.addEventListener('click', alertMethod);
+function alertMethod(this: HTMLElement, ev: Event) {
+  alert('Alertando pelo typescript');
+}
+
 // Write TypeScript code!
 const appTitle: HTMLElement = document.getElementById('app.title');
 appTitle.innerHTML = title;
 
+let greeter = new Greeter('Impiedoso');
+
 const appParagraph: HTMLElement = document.getElementById('app.paragraph');
-appParagraph.innerHTML = paragraph;
+appParagraph.innerHTML = greeter.getGreeting();
